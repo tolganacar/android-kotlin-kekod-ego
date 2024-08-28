1770400fc95955a9e93bf34120847ee621bddc76
Tolga Nacar <tolga_nacar@outlook.com>
2024-08-28 17:50:16 +0300

    Create switch fragments

---
+++ KekodEgo/app/src/main/java/com/tolganacar/kekodego/ui/HappinessFragment.kt
@@ -0,0 +1,18 @@
+package com.tolganacar.kekodego.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.tolganacar.kekodego.R
+
+class HappinessFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_happiness, container, false)
+    }
+}
